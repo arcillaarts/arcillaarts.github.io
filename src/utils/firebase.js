7569e9681a978c2b224c18cc177900e084b2d566
Arcilla Arts <arcillaarts@example.com>
2026-09-07 14:15:10 +0530

    Initialize Firebase Analytics to track visitor data

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
-export { db };
+export { db, analytics };
